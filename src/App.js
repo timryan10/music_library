d5121c2f80ab2f3d4df6a7e0c7b82f42c8460e59
Tim <rtim10@ymail.com>
2023-11-22 19:41:16 -0500

    fetch music data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ function App() {
   const [search, setSearch] = useState('')
   const [message, setMessage] = useState('Search for music')
   const [data, setData] = useState([])
+
+useEffect(() => {
+  const fetchData = async () => {
+    const url = 'https://itunes.apple.com/search?term=black%20sabbath'
+    const response = await fetch(url)
+    const data = await response.json()
+
+    if (data.results) {
+      setData(data.results)
+    } else{
+      setData([])
+      setMessage('Not Found')
+    }
+  }
+
+  fetchData()
+}, [search])
+
   return (
     <div className="App">
       <Gallery />
